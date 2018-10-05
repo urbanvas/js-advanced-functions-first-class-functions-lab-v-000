6260da95596ce4cc208b8da5377d879f13db0a06
Neil Seejoor <nvseejoor@gmail.com>
2018-10-05 02:09:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const returnLastTwoDrivers = function (arr) {
 }
 
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
