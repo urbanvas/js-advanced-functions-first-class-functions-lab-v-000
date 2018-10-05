6283463f9eee2d7ecc140cc845d6e892f8642140
Neil Seejoor <nvseejoor@gmail.com>
2018-10-05 02:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function (drivers, whichDrivers) {
+const fetchSpecifiedDrivers = function (drivers, whichDrivers) {
   return whichDrivers(drivers);
 };
